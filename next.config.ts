ae38bb13c1acfef2f168384999d7227844982fb0
OwnIt Developer <developer@ownit.app>
2026-04-16 01:09:22 +0200

    chore: configure nextjs export and github actions for GH Pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Configuration pour le déploiement sur GitHub Pages avec le nom du dépôt
+  basePath: '/Parcours_le.cartel',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
